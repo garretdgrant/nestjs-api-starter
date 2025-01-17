e020450843013e6e6a73afe8d02db0dc302f4437
Garret Grant <garret.grant.swe@gmail.com>
2025-01-17 13:16:28 -0800

    chore: add min test cov and pr template

--- jest.config.ts
+++ jest.config.ts
@@ -8,17 +8,25 @@ const config: Config = {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: [
-    '**/*.(t|j)s', // Include only TypeScript and JavaScript files
-    '!**/node_modules/**', // Exclude node_modules
-    '!**/dist/**', // Exclude build artifacts
-    '!**/src/main.ts', // Exclude main.ts if coverage for it isn't critical
-    '!**/*.module.ts', // Exclude module files as they are mostly configuration
+    '**/*.(t|j)s',          // Include only TypeScript and JavaScript files
+    '!**/node_modules/**',  // Exclude node_modules
+    '!**/dist/**',          // Exclude build artifacts
+    '!**/src/main.ts',      // Exclude main.ts if coverage for it isn't critical
+    '!**/*.module.ts',      // Exclude module files as they are mostly configuration
   ],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
   coveragePathIgnorePatterns: [
-    '/src/main.ts'
-  ]
+    '/src/main.ts', // Ignore main.ts in coverage
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 85,    // Minimum branch coverage
+      functions: 85,   // Minimum function coverage
+      lines: 85,       // Minimum line coverage
+      statements: 85,  // Minimum statement coverage
+    },
+  },
 };
 
 export default config;
